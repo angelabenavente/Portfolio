6907f11b68e8ea8f392985d5099036d37156510a
angelabenavente <angela.b.almazan@gmail.com>
2019-12-24 20:51:49 +0100

    Add initial template

---
+++ _src/assets/js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+console.log('>> Ready :)');
+
+
+$('.menu-burger, .menu-items').on('click', function() {
+  $('.menu-bg, .menu-items, .menu-burger').toggleClass('fs');
+  $('.menu-burger').text() == "☰" ? $('.menu-burger').text('✕') : $('.menu-burger').text('☰');
+});
---
+++ public/assets/js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+console.log('>> Ready :)');
+
+
+$('.menu-burger, .menu-items').on('click', function() {
+  $('.menu-bg, .menu-items, .menu-burger').toggleClass('fs');
+  $('.menu-burger').text() == "☰" ? $('.menu-burger').text('✕') : $('.menu-burger').text('☰');
+});
+//# sourceMappingURL=main.js.map
