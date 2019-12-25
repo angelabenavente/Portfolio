b888f7a7c7bc6df618ba8275ef8923d2f46e67cc
angelabenavente <angela.b.almazan@gmail.com>
2019-12-25 12:42:46 +0100

    Add collapsibles to formation

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,10 +1,196 @@
 'use strict';
 
 console.log('>> Ready :)');
+//$('.menu-burger, .menu-items').on('click', function() {
+  //$('.menu-bg, .menu-items, .menu-burger').toggleClass('fs');
+  //$('.menu-burger').text() == "☰" ? $('.menu-burger').text('✕') : $('.menu-burger').text('☰');
+//});
 
+'use strict';
 
-$('.menu-burger, .menu-items').on('click', function() {
-  $('.menu-bg, .menu-items, .menu-burger').toggleClass('fs');
-  $('.menu-burger').text() == "☰" ? $('.menu-burger').text('✕') : $('.menu-burger').text('☰');
-});
+// TOOGLE PALETA COLORES 
+const designArrowButton = document.querySelector('.bootcamp-arrow-button');
+const div = document.querySelector('#js-toggle');
+const elementHidden = document.querySelector('#toggle');
+
+function hideNotDesignElements () {
+  form.classList.add('hidden');
+  arrowActiveForm.classList.remove('fa-chevron-up');
+  arrowActiveForm.classList.add('fa-chevron-down');
+  formationCardSection.classList.add('hidden');
+  arrowActiveShare.classList.remove('fa-chevron-up');
+  arrowActiveShare.classList.add('fa-chevron-down');
+  coursesHidden.classList.add('hidden'); 
+  coursesArrowButton.classList.remove('fa-chevron-up');
+  coursesArrowButton.classList.add('fa-chevron-down');
+  //shareCardButton.classList.add('hidden')
+}
+
+function toggle () {
+  
+  if (elementHidden.classList.contains('hidden')) {
+  hideNotDesignElements ()
+    div.style.border = "none";
+    elementHidden.classList.remove('hidden');
+    designArrowButton.classList.remove('fa-chevron-down');
+    designArrowButton.classList.add('fa-chevron-up');
+    formationIntroElement.style.borderBottom = "2px solid #33323242";
+    courses.style.borderBottom = "2px solid #33323242";
+    headerForm.style.borderBottom = "2px solid #33323242";
+    coursesHidden.classList.add('hidden'); 
+  coursesArrowButton.classList.remove('fa-chevron-up');
+  coursesArrowButton.classList.add('fa-chevron-down');
+  } else {
+    elementHidden.classList.add('hidden'); 
+    div.style.borderBottom = "2px solid #33323242";
+    designArrowButton.classList.remove('fa-chevron-up');
+    designArrowButton.classList.add('fa-chevron-down');
+    coursesHidden.classList.add('hidden');
+  }
+}
+
+div.addEventListener('click', toggle);
+
+//Desplegar formulario
+const arrowActiveForm = document.querySelector('.arrow-button-form');
+const form = document.querySelector('.master__form')
+const headerForm = document.querySelector('.master__header-container')
+const formationmaster = document.querySelector('.formation__fill');
+
+function hideNotFormElements () {
+  elementHidden.classList.add('hidden');
+  div.style.borderBottom = "2px solid #33323242";
+  designArrowButton.classList.remove('fa-chevron-up');
+  designArrowButton.classList.add('fa-chevron-down');
+  formationCardSection.classList.add('hidden');
+  arrowActiveShare.classList.remove('fa-chevron-up');
+  arrowActiveShare.classList.add('fa-chevron-down');
+  formationIntroElement.style.borderBottom = "2px solid #33323242";
+  courses.style.borderBottom = "2px solid #33323242";
+  coursesHidden.classList.add('hidden'); 
+  coursesArrowButton.classList.remove('fa-chevron-up');
+  coursesArrowButton.classList.add('fa-chevron-down');
+  //shareCardButton.classList.add('hidden')
+}
+
+function showOrHideForm () {
+  hideNotFormElements()
+  if (form.classList.contains('hidden')) {
+    form.classList.remove('hidden');
+    headerForm.style.border = "none";
+    arrowActiveForm.classList.remove('fa-chevron-down');
+    arrowActiveForm.classList.add('fa-chevron-up');
+  } else {
+    form.classList.add('hidden');
+    headerForm.style.borderBottom = "2px solid #33323242";
+    arrowActiveForm.classList.remove('fa-chevron-up');
+    arrowActiveForm.classList.add('fa-chevron-down');
+    coursesArrowButton.classList.remove('fa-chevron-up');
+    coursesArrowButton.classList.add('fa-chevron-down');
+    courses.style.borderBottom = "2px solid #33323242";
+  }
+}
+
+headerForm.addEventListener('click', showOrHideForm);
+
+//Desplegar share card
+const formationButton = document.querySelector('.formation-button');
+const shareSection = document.querySelector('.formation__share');
+const formationIntroElement = document.querySelector('.formation-container');
+const arrowActiveShare = document.querySelector('.arrow-button-share');
+const formationCardButton = document.querySelector('#formation-button');
+const formationCardSection = document.querySelector('#formation-button-section');
+const shareCardButton =  document.querySelector("#share-card");
+const validateInputsSection = document.querySelector(".validateInputsSection");
+
+function hiddeNotShareElements() {
+  form.classList.add('hidden');
+  headerForm.style.borderBottom = "2px solid #33323242";
+  div.style.borderBottom = "2px solid #33323242";
+  designArrowButton.classList.remove('fa-chevron-up');
+  designArrowButton.classList.add('fa-chevron-down');
+  elementHidden.classList.add('hidden');
+  formationIntroElement.style.borderBottom = "2px solid #33323242";
+  arrowActiveForm.classList.remove('fa-chevron-up');
+  arrowActiveForm.classList.add('fa-chevron-down');
+  courses.style.borderBottom = "2px solid #33323242";
+  coursesHidden.classList.add('hidden'); 
+  coursesArrowButton.classList.remove('fa-chevron-up');
+  coursesArrowButton.classList.add('fa-chevron-down');
+}
+
+function showOrHideShare() {
+  hiddeNotShareElements();
+  if (formationCardSection.classList.contains('hidden')){
+    formationCardSection.classList.remove('hidden');
+    formationIntroElement.style.border = "none";
+    arrowActiveShare.classList.remove('fa-chevron-down');
+    arrowActiveShare.classList.add('fa-chevron-up');
+  } else {
+    formationCardSection.classList.add('hidden');
+    formationIntroElement.style.borderBottom = "2px solid #33323242";
+    arrowActiveShare.classList.remove('fa-chevron-up');
+    arrowActiveShare.classList.add('fa-chevron-down');
+    shareCardButton.classList.add('hidden');
+    coursesArrowButton.classList.remove('fa-chevron-up');
+    coursesArrowButton.classList.add('fa-chevron-down');
+    coursesHidden.classList.add('hidden'); 
+    coursesArrowButton.classList.remove('fa-chevron-up');
+    coursesArrowButton.classList.add('fa-chevron-down');
+  }
+  //validateInputs(); 
+}
+
+formationIntroElement.addEventListener('click', showOrHideShare);
+
+//Desplegar courses card
+
+const coursesArrowButton = document.querySelector('.courses-arrow-button');
+const courses = document.querySelector('#js-toggle-courses');
+const coursesHidden = document.querySelector('#toggle-courses');
+
+function hideNotDesignElements () {
+  form.classList.add('hidden');
+  arrowActiveForm.classList.remove('fa-chevron-up');
+  arrowActiveForm.classList.add('fa-chevron-down');
+  formationCardSection.classList.add('hidden');
+  arrowActiveShare.classList.remove('fa-chevron-up');
+  arrowActiveShare.classList.add('fa-chevron-down');
+  formationIntroElement.style.borderBottom = "2px solid #33323242";
+  headerForm.style.borderBottom = "2px solid #33323242";
+  elementHidden.style.borderBottom = "2px solid #33323242";
+  div.style.borderBottom = "2px solid #33323242";
+  //shareCardButton.classList.add('hidden')
+}
+
+function toggleCourses () {
+  hideNotDesignElements ()
+  if (coursesHidden.classList.contains('hidden')) {
+      coursesHidden.classList.remove('hidden');
+      courses.style.border = "none";
+      coursesArrowButton.classList.remove('fa-chevron-down');
+      coursesArrowButton.classList.add('fa-chevron-up');
+      elementHidden.classList.add('hidden'); 
+      shareCardButton.classList.add('hidden');
+      formationCardSection.classList.add('hidden');
+      formationIntroElement.style.borderBottom = "2px solid #33323242";
+      elementHidden.style.borderBottom = "2px solid #33323242";
+      headerForm.style.borderBottom = "2px solid #33323242";
+  } else {
+      coursesHidden.classList.add('hidden'); 
+      courses.style.borderBottom = "2px solid #33323242";
+      coursesArrowButton.classList.remove('fa-chevron-up');
+      coursesArrowButton.classList.add('fa-chevron-down');
+  }
+}
+
+courses.addEventListener('click', toggleCourses);
+
+//Desplegar ventana de compartir card
+function removeClass () {
+  shareCardButton.classList.remove('hidden');
+}
+
+//formationCardButton.addEventListener('click',removeClass);
+
 //# sourceMappingURL=main.js.map
